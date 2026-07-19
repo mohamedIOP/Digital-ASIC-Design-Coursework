8a19f265c2164c108c7d1d3a6db7592f1cd59176
mohamedIOP <mohamediop32@outlook.com>
2026-07-19 08:44:05 +0300

    Solve Tcl Assignment One

---
+++ Assignments/TCL Assignments/Assignment_TCL_1/Ass_1_2.tcl
@@ -0,0 +1,7 @@
+puts "****Mathematical Operations****";
+set a -64;
+set b 49;
+set var0 [expr abs($a)];
+set var1 [expr sqrt($b)];
+puts "The Content Of Var0 is $var0";
+puts "The Content Of Var1 is $var1";
