9dc98dc3e1d48000fede1a28dfb6baef8baf49c6
mohamedIOP <mohamediop32@outlook.com>
2026-08-06 01:39:05 +0300

    Optimize Parity_Calc UART_TX Assignment

--- Assignments/Verilog Assignments/UART/TX/parity_Calc.v
+++ Assignments/Verilog Assignments/UART/TX/parity_Calc.v
@@ -4,9 +4,6 @@ module Parity_Calc(
     input CLK,RST,
     output reg parBit
 );
-    // Calculating the number of odd ones
-    wire oddCount;
-    assign oddCount = ^pData;
     always @(posedge CLK or negedge RST) begin
         if (!RST) parBit <= 0;
         else if (regData) begin
