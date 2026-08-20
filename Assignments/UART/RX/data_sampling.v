819d86016703d79c890d64dfd158a90f8e9d2840
mohamedIOP <mohamediop32@outlook.com>
2026-08-20 16:22:19 +0300

    Add data_sampling block UART_RX Assignment

---
+++ Assignments/UART/RX/data_sampling.v
@@ -0,0 +1,27 @@
+module data_sampling (
+    input RX_IN,
+    input data_samp_en,
+    input CLK,RST,
+    output sampled_bit
+);
+    reg [1:0] inner_counter;
+    // Sample three bits
+    reg [2:0] majority_reg;
+    always @(posedge CLK or negedge RST) begin
+        if (!RST) begin
+            majority_reg  <= 0;
+            inner_counter <= 0;
+        end
+        else if (!data_samp_en) begin
+            majority_reg  <= 0;
+            inner_counter <= 0;
+        end
+        else if (inner_counter < 3) begin
+            majority_reg[inner_counter] <= RX_IN;
+            inner_counter <= inner_counter + 1;
+        end
+        // else: data_samp_en still high but we already have 3 samples — hold, do nothing
+    end
+    // Calualate the majority bit
+    assign sampled_bit = (majority_reg[0] && majority_reg[1]) || (majority_reg[0] && majority_reg[2]) || (majority_reg[1] && majority_reg[2]);
+endmodule //data_sampling
