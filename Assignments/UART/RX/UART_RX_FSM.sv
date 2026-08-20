3842c966966241770f1282107d74c00126b80973
mohamedIOP <mohamediop32@outlook.com>
2026-08-20 15:56:10 +0300

    Add Comments to FSM Block UART_RX Assignment

--- Assignments/UART/RX/UART_RX_FSM.sv
+++ Assignments/UART/RX/UART_RX_FSM.sv
@@ -12,17 +12,18 @@ module UART_Rx_FSM (
     output reg dat_samp_en,
     output reg data_valid
 );
-    wire bit_transition = edge_cnt == prescale - 1;
-    wire [4:0] middleCnt = prescale >> 1; 
-    wire data_sampled_flag = edge_cnt == middleCnt + 2;
+    // flags needed for the operation
+    wire bit_transition = edge_cnt == prescale - 1; //RX_IN will send another bit
+    wire [4:0] middleCnt = prescale >> 1; // we have reached the middle of the cycles during one bit of RX_IN 
+    wire data_sampled_flag = edge_cnt == middleCnt + 2; // We have sampled the bit from data_sampling block
     // Define the states 
     typedef enum logic [2:0] {
-        idle  = 'b000,
-        start = 'b001,
-        data  = 'b011,
-        parity = 'b010,
-        stop = 'b110,
-        valid = 'b100
+        idle  = 'b000, // waiting RX_IN to be Zero
+        start = 'b001, // Checking that is Start bit
+        data  = 'b011, // Getting the Data on the P_DATA Bus also assert deser_en
+        parity = 'b010, // check parity bit is correct or not
+        stop = 'b110, // check the stop bit is correct or not
+        valid = 'b100 // check the data is frame is correct or not
 } state_t;
     state_t currentState,nextState;
     always @(posedge CLK or negedge RST) begin
@@ -34,57 +35,57 @@ module UART_Rx_FSM (
         end
     end
     always @(*) begin
-        nextState = idle;
+        nextState = idle; // set the default to be idle state
         case (currentState)
             idle: 
                 if (!RX_IN) begin
-                    nextState = start;
+                    nextState = start; // ready to start getting the frame
                 end
                 else begin
-                    nextState = idle;
+                    nextState = idle; // no frame is being recieved
                 end
             start:
-                if(bit_transition && !strt_glitch) begin
+                if(bit_transition && !strt_glitch) begin // the start bit recieved is correct and we will get the data bits 
                     nextState = data;
                 end
-                else if (!RX_IN && strt_glitch && bit_transition) begin
+                else if (!RX_IN && strt_glitch && bit_transition) begin // the start bit recieved is incorrect but there is another frame will start
                     nextState = start;
                 end
-                else if (RX_IN && strt_glitch && bit_transition) begin
+                else if (RX_IN && strt_glitch && bit_transition) begin // the start bit recieved is incorrect and we will be idle again
                     nextState = idle;
                 end
                 else begin
-                    nextState = start;
+                    nextState = start; // stay here until the bit is sampled and have got all edges
                 end
             data:
-                if (PAR_EN && (bit_cnt == 8)) begin
-                    nextState = parity;
+                if (PAR_EN && (bit_cnt == 8)) begin // Collected the data and there is parity bit so go to parity state
+                    nextState = parity; 
                 end
-                else if (!PAR_EN && (bit_cnt == 8)) begin
+                else if (!PAR_EN && (bit_cnt == 8)) begin // Collected the data and there isn't parity bit so go to stop state
                     nextState = stop;
                 end
                 else begin
-                    nextState = data;
+                    nextState = data; // Collecting the data rightnow
                 end
             parity:
-                if (bit_transition) begin
-                    nextState = stop;
+                if (bit_transition) begin // we have checked the parity bit and will go to stop state
+                    nextState = stop; 
                 end
                 else begin
-                    nextState = parity;
+                    nextState = parity; // Checking parity bit
                 end
             stop:
-                if (bit_transition) begin
+                if (bit_transition) begin // Done Checking stop state and move to valid state
                     nextState = valid;
                 end
                 else begin
-                    nextState = stop;
+                    nextState = stop; // Checking stop state
                 end
             valid:
-                if (RX_IN) begin
-                    nextState = idle;
+                if (RX_IN) begin // return to idle  
+                    nextState = idle; 
                 end
-                else if (!RX_IN) begin
+                else if (!RX_IN) begin // return to start
                     nextState = start;
                 end
                 else begin
