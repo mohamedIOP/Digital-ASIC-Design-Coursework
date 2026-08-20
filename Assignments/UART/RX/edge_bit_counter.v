976c4806215c9bfb22bdb71e3a2702aabd58695f
mohamedIOP <mohamediop32@outlook.com>
2026-08-20 15:35:57 +0300

    Add edge_bit__counter block UART_RX Assignment

---
+++ Assignments/UART/RX/edge_bit_counter.v
@@ -0,0 +1,27 @@
+module edge_bit_counter (
+    input edge_bit_cnt_enable,
+    input [5:0] prescale,
+    input CLK,RST,
+    output reg [4:0] edge_cnt,
+    output reg [3:0] bit_cnt
+);
+    always @(posedge CLK or negedge RST) begin
+        if (!RST) begin
+            edge_cnt <= 0;
+            bit_cnt <= 0;
+        end
+        else if (!edge_bit_cnt_enable) begin
+            edge_cnt <= 0;
+            bit_cnt <= 0;
+        end
+        else begin
+            if (edge_cnt == prescale - 1) begin
+                edge_cnt <= 0;
+                bit_cnt <= bit_cnt + 1;
+            end
+            else begin
+                edge_cnt <= edge_cnt + 1;
+            end
+        end
+    end
+endmodule //edge_bit_counter
