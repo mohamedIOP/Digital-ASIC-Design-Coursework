ffa8910912a6373825357bbe81a79c8ec4ebb158
mohamedIOP <mohamediop32@outlook.com>
2026-08-21 18:55:02 +0300

    Add parity_Check block UART_RX Assignment

---
+++ Assignments/UART/RX/parity_Check.v
@@ -0,0 +1,16 @@
+module parity_Check (
+    input par_chk_en,PAR_TYP,sampled_bit,
+    input CLK,RST,
+    input [7:0] P_DATA,
+    output reg par_err
+);
+    wire calc_par_bit = (PAR_TYP) ? ~^P_DATA : ^P_DATA;
+    always @(posedge CLK or negedge RST) begin
+        if (!RST) begin
+            par_err <= 0;
+        end
+        else if (par_chk_en) begin
+            par_err <= (calc_par_bit ^ sampled_bit);
+        end
+    end
+endmodule //parity_Check
